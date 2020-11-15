ec91fb5eb128f45409933a29718b3532b2620a21
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2020-11-15 18:27:06 +0100

    Add files via upload

---
+++ Varijable vježba 4/Varijable vježba 4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable_vježba_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string upis;
+                Console.Write("Unesite rečenicu: ");
+                upis = Console.ReadLine();
+                string sVelika = upis.ToUpper();
+                string sMala = upis.ToLower();
+                string sPrvaTri = upis.Substring(0, 3);
+                string sZadnjihPet = upis.Substring(upis.Length - 5);
+                string s8_11 = upis.Substring(7, 4);
+                
+                Console.WriteLine(sVelika);
+                Console.WriteLine(sMala);
+                Console.WriteLine(sPrvaTri);
+                Console.WriteLine(sZadnjihPet);
+                Console.WriteLine(s8_11);
+                
+                
+                
+                Console.ReadKey();
+            }
+        }
+    }
+}
